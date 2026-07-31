f2a7a82fd6c12fa055fecbb3d7468ad3c7c31365
Sercan <sercansimsek44@gmail.com>
2026-07-31 14:59:13 +0200

    CORS

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,14 @@ const express = require("express");
 const User = require("../models/user");
 const passport = require("passport");
 const authenticate = require("../authenticate");
+const cors = require("./cors");
 
 const router = express.Router();
 
 /* GET users listing. */
 router.get(
   "/",
+  cors.corsWithOptions,
   authenticate.verifyUser,
   authenticate.verifyAdmin,
   (req, res, next) => {
@@ -21,7 +23,7 @@ router.get(
   },
 );
 
-router.post("/signup", (req, res) => {
+router.post("/signup", cors.corsWithOptions, (req, res) => {
   const user = new User({ username: req.body.username });
 
   User.register(user, req.body.password)
@@ -50,6 +52,7 @@ router.post("/signup", (req, res) => {
 
 router.post(
   "/login",
+  cors.corsWithOptions,
   passport.authenticate("local", { session: false }),
   (req, res) => {
     const token = authenticate.getToken({ _id: req.user._id });
@@ -63,7 +66,7 @@ router.post(
   },
 );
 
-router.get("/logout", (req, res, next) => {
+router.get("/logout", cors.corsWithOptions, (req, res, next) => {
   if (req.session && req.session.user) {
     // Check for a specific user property
     req.session.destroy((err) => {
